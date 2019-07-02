65b5c29f0eb7d025fec44baa201e026129b7d0f4
Clark Lin <clarklindeveloper@gmail.com>
2019-07-02 08:30:52 +0200

    08. Fetching Data on Update (without Creating Infinite Loops).mp4

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -9,12 +9,13 @@ import axios from 'axios';
 
 class Blog extends Component {
 	state = {
-		posts: []
+		posts: [],
+		selectedPostId: null
 	};
 
 	componentDidMount() {
 		axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
-			// console.log(response.data);
+			console.log(response.data);
 			const posts = response.data.slice(0, 4);
 			const updatedPosts = posts.map(post => {
 				return {
@@ -25,15 +26,27 @@ class Blog extends Component {
 			this.setState({ posts: updatedPosts });
 		});
 	}
+
+	postSelectedHandler = id => {
+		this.setState({ selectedPostId: id });
+	};
+
 	render() {
 		const posts = this.state.posts.map(post => {
-			return <Post key={post.id} title={post.title} author={post.author} />;
+			return (
+				<Post
+					key={post.id}
+					title={post.title}
+					author={post.author}
+					clicked={() => this.postSelectedHandler(post.id)}
+				/>
+			);
 		});
 		return (
 			<div>
 				<section className="Posts">{posts}</section>
 				<section>
-					<FullPost />
+					<FullPost id={this.state.selectedPostId} />
 				</section>
 				<section>
 					<NewPost />
