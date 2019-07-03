76c502b4cd58d0b241cf105af22ceeefe28debf4
Clark Lin <clarklindeveloper@gmail.com>
2019-07-03 08:36:19 +0200

    moved Aux into Auxiliary folder

---
+++ burger-basics--01-project-setup/src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+import Aux from '../../Auxiliary'
+const withErrorHandler = (WrappedComponent) => {
+  return (props)=>{
+    return (
+
+    );  
+  }
+}
+
+export default withErrorHandler;
