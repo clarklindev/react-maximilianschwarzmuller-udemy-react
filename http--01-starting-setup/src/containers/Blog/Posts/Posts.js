31b1189e3a5fc6e5105da72f7cb6789993448f69
Clark Lin <clarklindeveloper@gmail.com>
2019-07-07 16:25:33 +0200

    react - routing - Navigating Programmatically

--- http--01-starting-setup/src/containers/Blog/Posts/Posts.js
+++ http--01-starting-setup/src/containers/Blog/Posts/Posts.js
@@ -36,13 +36,14 @@ class Posts extends Component {
 		if (!this.state.error) {
 			posts = this.state.posts.map(post => {
 				return (
-					<Link to={'/' + post.id} key={post.id}>
-						<Post
-							title={post.title}
-							author={post.author}
-							clicked={() => this.postSelectedHandler(post.id)}
-						/>
-					</Link>
+					// <Link to={'/' + post.id} key={post.id}> //removing to show how to navigate programatically
+					<Post
+						key={post.id}
+						title={post.title}
+						author={post.author}
+						clicked={() => this.postSelectedHandler(post.id)}
+					/>
+					// </Link>
 				);
 			});
 		}
