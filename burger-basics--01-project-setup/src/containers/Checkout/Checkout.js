73b5bdd9eca528a069437b2cf33c4a4d7a37dc2f
Clark Lin <clarklindeveloper@gmail.com>
2019-07-24 06:42:10 +0200

    05. Navigating Back & To Next Page.mp4

--- burger-basics--01-project-setup/src/containers/Checkout/Checkout.js
+++ burger-basics--01-project-setup/src/containers/Checkout/Checkout.js
@@ -4,10 +4,23 @@ class Checkout extends Component {
 	state = {
 		ingredients: { salad: 1, meat: 1, cheese: 1, bacon: 1 }
 	};
+
+	checkoutCancelledHandler = () => {
+		this.props.history.goBack();
+	};
+
+	checkoutContinuedHandler = () => {
+		this.props.history.replace('/checkout/contact-data');
+	};
+
 	render() {
 		return (
 			<div>
-				<CheckoutSummary ingredients={this.state.ingredients} />
+				<CheckoutSummary
+					ingredients={this.state.ingredients}
+					checkoutCancelled={this.checkoutCancelledHandler}
+					checkoutContinued={this.checkoutContinuedHandler}
+				/>
 			</div>
 		);
 	}
